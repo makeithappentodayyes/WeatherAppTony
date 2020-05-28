df39191a38a72952773eafcd93dab9c8b52aa726
Daniel Gil <danielgilucsd@gmail.com>
2020-05-28 13:09:44 -0700

    received data weather from manchester

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,18 +1,19 @@
-const key = '9AthK4nAQQnK2qlDsKTySJLmGxHbaoPv'
+const key = "9AthK4nAQQnK2qlDsKTySJLmGxHbaoPv";
 
-// geth weather infomation
+// get weather information
+
 const getWeather = async (id) => {
-    
-    const base =  'http://dataservice.accuweather.com/currentconditions/v1/';
-    const query = `${id}?apikey=$w{key}`;    
-    
-    const response = await fetch(base + query);
 
+    const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch( base + query);
     const data = await response.json();
 
-    console.log(data);
-    
+    return data[0];
+
 };
+
 
 // get city information
 const getCity = async (city) => {
@@ -20,15 +21,17 @@ const getCity = async (city) => {
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
     const query = `?apikey=${key}&q=${city}`;
 
-    const response = await  fetch(base + query);
+    const response = await fetch(base + query);
     const data = await response.json();
 
-    return (data[0]);
+    return data[0];
+
+
 };
 
-// getCity('manchester')
-//     .then(data => console.log(data))
-//     .catch(err => console.log(err));
+getCity('manchester').then(data => {
+        return getWeather(data.Key)
+    }).then(data => {
+        console.log(data);
+    }).catch(err => console.log(err));
 
-
-getWeather('329260'); 
